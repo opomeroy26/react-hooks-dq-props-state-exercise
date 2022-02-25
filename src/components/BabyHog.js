75bcac6bbd295c6caad34bc58bf413bb4fb45a1a
Olivia Pomeroy <opomeroy26@gmail.com>
2022-02-25 10:15:33 -0800

    commit tree 9750cde9058ed18d8a15fd46a916711665da1139

--- src/components/BabyHog.js
+++ src/components/BabyHog.js
@@ -6,6 +6,11 @@ import GlowingBaby from "../assets/glowing-eyes.png";
 
 // is there a way we could associate eye color string values with images?
 // perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
+const eyeColorMapper = {
+  blue: BlueBaby,
+  sun: SunBaby,
+  glowing: GlowingBaby,
+}
 
 function BabyHog(props) {
   function handleChangeWeight(e) {
@@ -15,17 +20,17 @@ function BabyHog(props) {
 
   return (
     <li className="hogbabies">
-      <h1>Name</h1>
+      <h1>{props.name}</h1>
       <h3>Weight:</h3>
-      <h3>Hobby:</h3>
-      <h4>Eye Color:</h4>
+      <h3>{props.hobby}:</h3>
+      <h4>{props.eyeColor}:</h4>
 
       <button name="+">Increase Weight</button>
       <button name="-">Decrease Weight</button>
 
       <div className="hb-wrap">
         <img
-          src={normalBaby}
+          src={eyeColorMapper[props.eyeColor]}
           style={{ height: "200px" }}
           alt="MasterBlasterJrJr"
         />
